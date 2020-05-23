d10a7558daf98d42f94f7d1336a75f661204cb5b
AllenWorker <allen88038@gmail.com>
2020-05-23 10:15:44 +0800

    add Store2

---
+++ src/main/java/com/lab/ocp/day05/Store2.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.lab.ocp.day05;
+
+/**
+ *
+ * @author MB-study
+ */
+public class Store2 {
+    public static void main(String[] args) {
+        Hamburger h = new Hamburger("大麥克", 80);
+        Hamburger h2 = new Hamburger("麥香魚", 45);
+    }
+}
