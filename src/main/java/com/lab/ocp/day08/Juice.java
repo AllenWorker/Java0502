5deab00ade8660361bced96fa8d09a32defbd49b
AllenWorker <allen88038@gmail.com>
2020-06-06 09:57:44 +0800

    commit

---
+++ src/main/java/com/lab/ocp/day08/Juice.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.lab.ocp.day08;
+
+/**
+ *
+ * @author MB-study
+ */
+public class Juice implements Drink{
+    private String name;
+    private Integer price;
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public Integer getPrice() {
+        return price;
+    }
+
+    @Override
+    public void setPrice(Integer price) {
+        this.price = price;
+    }
+    
+}
